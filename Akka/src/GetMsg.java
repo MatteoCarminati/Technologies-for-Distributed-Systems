aaea1dea730e519f4a4a4d0dc722d86e947c0f1d
Lorenzo Torri <lrnztrr0@gmail.com>
2023-11-16 12:05:21 +0100

    Create GetMsg.java

---
+++ Akka/src/GetMsg.java
@@ -0,0 +1,13 @@
+package com.Ex5;
+
+public class GetMsg {
+    private String name;
+
+    public GetMsg(String name){
+        this.name = name;
+    } 
+
+    public String getName() {
+        return this.name;
+    }
+}
